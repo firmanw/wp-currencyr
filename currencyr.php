66bb597e5c01c9795f257eda677c3337cfea1900
Firman Wandayandi <firmanw@gmail.com>
2012-12-18 02:26:50 +0700

    Fix the Plugin URI and increase the version to 1.0.1

--- currencyr.php
+++ currencyr.php
@@ -1,16 +1,16 @@
 <?php
 /*
 Plugin Name: Currencyr
-Plugin URI: http://adivalabs.com/wordpress-plugins/currencyr
+Plugin URI: http://adivalabs.com/currencyr
 Description: An easy, simple yet advance currency converter. Support Yahoo!, Google, Open Exchange Rates, FoXRate and European Central Bank exchange rates service, currency exchange rates list and converter widget, shortcode, WP Cron schedule, cache, custom currency and ip to local currency.
-Version: 1.0
+Version: 1.0.1
 Author: Firman Wandayandi
 Author URI: http://firmanw.com/
 Text Domain: currencyr
 Domain Path: /languages
 */
 
-define( 'CURRENCYR_VERSION', '1.0' );
+define( 'CURRENCYR_VERSION', '1.0.1' );
 define( 'CURRENCYR_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 define( 'CURRENCYR_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'CURRENCYR_PLUGIN_ASSETS', plugin_dir_url( __FILE__ ) . 'assets' );
